8d57f5cadbd6cee69333fe025b3bd29b72ec6ea4
Sven Paalberg <svenpaa@greeny.cs.tlu.ee>
2015-09-11 13:30:18 +0300

    login.php

--- hello_world.php
+++ hello_world.php
@@ -7,6 +7,9 @@
 	echo $first_name." ".$last_name;
 	
 ?>
+
+<br/>
+
 <?php
 	$age = 5;
 	
@@ -21,3 +24,36 @@
 			echo "täisealine";
 		}
 ?>
+
+<?php
+	// nii mitu korda trüki välja "palju ", kui palju on vanust
+	
+	for($i = 0; $i < $age; $i = $i + 1) {
+		
+		// see mida korrutatakse
+		echo "palju ".$i." ";
+		
+	}
+
+	echo "õnne";
+	
+	
+?>
+
+<br/>
+
+<?php
+
+	// kuupäev kujul: Friday, 11.09.2015
+	echo( date("l, j.m.y") );
+
+
+
+
+
+?>
+
+
+
+
+
