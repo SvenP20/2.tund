c633deefc53957a5bb940d002bc8530e0aeaacf9
Sven Paalberg <svenpaa@greeny.cs.tlu.ee>
2015-09-11 12:01:16 +0300

    first commit

---
+++ hello_world.php
@@ -0,0 +1,23 @@
+<?php
+
+	$first_name = "Sven";
+	$last_name = "Paalberg";
+	
+
+	echo $first_name." ".$last_name;
+	
+?>
+<?php
+	$age = 5;
+	
+	//kui on väiksem kui 18, siis alaealine
+	//kui on suurem siis täisealine
+	if($age<18) {
+		//tõene
+		echo "alaealine";
+		}
+		
+		else {
+			echo "täisealine";
+		}
+?>
